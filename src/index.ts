322abd1f169561953432ef7127867428702760c0
alisson-bucchi <alisson.bucchi@gmail.com>
2023-12-11 22:05:52 +0000

    Buscar pagina html e renderizar no elemento root #1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
-console.log("Hello World!");
+
+const renderPages = (pageName: string): void => {
+    fetch(`pages/${pageName}.html`)
+      .then((resp) => resp.text())
+      .then((html) => {
+        const rootElem = document.getElementById("root") as HTMLElement;
+        rootElem.innerHTML = html;
+      });
+}
+renderPages("start"); 
+renderPages("quiz");
+renderPages("leaderboard"); 
