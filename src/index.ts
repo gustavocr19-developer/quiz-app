2f9bbd666044b4e7fa5c8813f60f7f914e7fbc3e
GuilhermeMF <97535820+Lokito2845@users.noreply.github.com>
2023-12-11 22:18:04 +0000

    Buscar pagina html e renderizar no elemento root #1

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+const renderPage = async (pageName: string): Promise<void> => {
+
+    const html = await fetch(`pages/${pageName}.html`).then((resp) => resp.text());
+    
+    const rootElement = document.getElementById("root") as HTMLElement;
+
+    rootElement.innerHTML = html;
+  };
+  
