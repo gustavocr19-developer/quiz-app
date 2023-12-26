972241caa217f6366d1a9c8f9f9f8118e3bf3c6b
alisson-bucchi <alisson.bucchi@gmail.com>
2023-12-26 22:34:43 +0000

    capturar nome e armazenar no local storage

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,27 @@
-
 const renderPages = (pageName: string): void => {
-    fetch(`pages/${pageName}.html`)
-      .then((resp) => resp.text())
-      .then((html) => {
-        const rootElem = document.getElementById("root") as HTMLElement;
-        rootElem.innerHTML = html;
-      });
-}
-renderPages("start"); 
-renderPages("quiz");
-renderPages("leaderboard"); 
+  fetch(`pages/${pageName}.html`)
+    .then((resp) => resp.text())
+    .then((html) => {
+      const rootElem = document.getElementById("root") as HTMLElement;
+      rootElem.innerHTML = html;
+    });
+};
+renderPages("start");
+//renderPages("quiz");
+//renderPages("leaderboard");
+
+
+const saveData = (): void => {
+  const inputUser = document.getElementById("#input-name") as HTMLInputElement;
+  const playerName = inputUser.value; 
+
+  if (playerName.trim() !== '') {
+    localStorage.setItem('playerName', playerName); 
+
+    alert('bem vindo, ${playerName}!'); 
+  } else {
+    alert('Inserir nome!'); 
+  }
+
+};
+saveData(); 
