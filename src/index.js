8daab4c234eb2d3243c719bb62de6b01e3c45c81
GuilhermeMF <97535820+Lokito2845@users.noreply.github.com>
2024-01-12 22:41:43 +0000

    final.1.1
    final.1.1

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var renderPages = function (pageName) {
+    // Realiza uma requisição fetch para carregar o conteúdo da página HTML
     fetch("pages/".concat(pageName, ".html"))
         .then(function (resp) {
         if (!resp.ok) {
@@ -7,13 +8,18 @@ var renderPages = function (pageName) {
         return resp.text();
     })
         .then(function (html) {
+        // Obtém o elemento raiz onde a página será exibida
         var rootElem = document.getElementById("root");
         if (rootElem) {
+            // Substitui o conteúdo do elemento raiz pelo HTML carregado
             rootElem.innerHTML = html;
+            // Obtém o formulário de início (se existir)
             var formStart = document.getElementById("form-start");
+            // Adiciona um ouvinte de evento para o formulário de início
             if (formStart) {
                 formStart.addEventListener("submit", function (event) {
                     event.preventDefault();
+                    // Chama a função armazenarNome ao enviar o formulário
                     armazenarNome();
                 });
             }
@@ -25,50 +31,60 @@ var renderPages = function (pageName) {
                 console.log("A página leaderboard está pronta para ser usada!");
                 var leaderboardContainer = document.querySelector(".container-podium");
                 if (leaderboardContainer) {
-                    // Limpar o conteúdo atual
                     leaderboardContainer.innerHTML = "";
-                    // Obter o nome do usuário do histórico local
                     var nomeUsuario = localStorage.getItem("usuario");
-                    // Verificar se há um nome de usuário
                     if (nomeUsuario) {
-                        // Criar um elemento para o usuário no primeiro lugar
+                        // Cria um elemento para o usuário no primeiro lugar do pódio
                         var firstPlaceItem = document.createElement("div");
                         firstPlaceItem.className = "podium-item";
-                        firstPlaceItem.innerHTML = "\n        <div class=\"podium-rounded first-podium\">".concat(nomeUsuario.charAt(0).toUpperCase(), "</div>\n        <div>").concat(nomeUsuario, "</div>\n        <div>1/1</div>\n      ");
-                        // Adicionar o elemento ao container-podium
+                        firstPlaceItem.innerHTML = "\n                <div class=\"podium-rounded first-podium\">".concat(nomeUsuario
+                            .charAt(0)
+                            .toUpperCase()).concat(nomeUsuario.charAt(1).toUpperCase(), "\n                </div>\n                <div>").concat(nomeUsuario, "</div>\n                <div>1/1</div>\n              ");
+                        // Adiciona o elemento ao contêiner do pódio
                         leaderboardContainer.appendChild(firstPlaceItem);
                     }
                     else {
                         console.error("Nome de usuário não encontrado no histórico local.");
                     }
+                    // Limpa a div "remaining-list"
+                    var remainingList = document.querySelector(".remaining-list");
+                    if (remainingList) {
+                        remainingList.innerHTML = "";
+                    }
+                    else {
+                        console.error('Elemento com classe "remaining-list" não encontrado.');
+                    }
                 }
                 else {
                     console.error('Elemento com classe "container-podium" não encontrado.');
                 }
+                // Obtém o botão "Novo Jogo"
+                var novoJogo = document.querySelector(".btn-gold");
+                // Adiciona um ouvinte de evento ao botão "Novo Jogo"
+                if (novoJogo) {
+                    novoJogo.addEventListener("click", function () {
+                        // Ao clicar no botão "Novo Jogo", retorna à página "start"
+                        renderPages("start");
+                    });
+                }
             }
-            var novoJogo = document.querySelector(".btn-gold");
-            // Adicionando um ouvinte de evento ao botão "Novo Jogo"
-            if (novoJogo) {
-                novoJogo.addEventListener("click", function () {
-                    // Ao clicar no botão "Novo Jogo", retorna à página "start"
-                    renderPages("start");
-                });
-            }
-        }
-        else {
-            console.error('Elemento com ID "root" não encontrado.');
         }
     })
         .catch(function (error) {
+        // Trata erros durante a requisição fetch
         console.error("Erro durante a requisição fetch:", error);
     });
 };
-// Função para exibir mensagens
 renderPages("start");
+// Função para armazenar o nome do usuário no localStorage
 function armazenarNome() {
+    // Obtém o elemento de input para o nome do usuário
     var nomeUsuarioInput = document.getElementById("input-name");
+    // Verifica se o elemento de input existe e se o valor não está vazio após remover espaços em branco
     if (nomeUsuarioInput && nomeUsuarioInput.value.trim() !== "") {
+        // Obtém o valor do nome do usuário
         var nomeUsuario = nomeUsuarioInput.value;
+        // Armazena o nome do usuário no localStorage
         localStorage.setItem("usuario", nomeUsuario);
         console.log("Nome armazenado com sucesso!");
         renderPages("quiz");
@@ -80,82 +96,99 @@ function armazenarNome() {
 function carregarPergunta() {
     fetch("questions.json")
         .then(function (resp) {
+        // Verifica se a resposta da requisição foi bem-sucedida
         if (!resp.ok) {
             throw new Error("Erro na requisição fetch.");
         }
+        // Converte o conteúdo da resposta para formato JSON
         return resp.json();
     })
         .then(function (data) {
+        // Exibe no console os dados recebidos do JSON
         console.log("Dados do JSON recebidos:", data);
+        // Atualiza o HTML com a primeira pergunta do conjunto de perguntas
         atualizarHTML(data.questions[0]);
     })
         .catch(function (error) {
+        // Trata erros durante a requisição fetch
         console.error("Erro durante a requisição fetch:", error);
     });
 }
 function verificarResposta(selectedIndex, correctIndex) {
-    var pontosPorRespostaCorreta = 1; // Defina a quantidade de pontos por resposta correta
     if (selectedIndex === correctIndex) {
         alert("Resposta correta!");
-        // Adiciona pontos ao usuário
-        adicionarPontos(pontosPorRespostaCorreta);
     }
     else {
         alert("Resposta incorreta");
     }
     renderPages("leaderboard");
 }
-function adicionarPontos(pontos) {
-    // Obtém a pontuação atual do usuário do localStorage ou inicializa com 0 se não existir
-    var pontuacaoAtual = parseInt(localStorage.getItem("pontuacao") || "0", 10);
-    // Adiciona os pontos
-    var novaPontuacao = pontuacaoAtual + pontos;
-    // Armazena a nova pontuação no localStorage
-    localStorage.setItem("pontuacao", novaPontuacao.toString());
-}
 var selectedOptionIndex = null;
+// Função para atualizar o conteúdo HTML.
 function atualizarHTML(pergunta) {
+    // Obtém elementos HTML relevantes
     var questionNumberElement = document.querySelector(".question-number");
     var questionContainerElement = document.querySelector(".question-container");
     var btnListElement = document.querySelector(".btn-list");
     var nextBtnElement = document.getElementById("next-btn");
-    if (questionNumberElement && questionContainerElement && btnListElement && nextBtnElement) {
-        questionNumberElement.textContent = "1/10";
+    // Verifica se os elementos existem
+    if (questionNumberElement &&
+        questionContainerElement &&
+        btnListElement &&
+        nextBtnElement) {
+        // Define o número da pergunta atual
+        questionNumberElement.textContent = "1/1";
+        // Define o texto da pergunta
         questionContainerElement.textContent = pergunta.question;
+        // Limpa a lista de botões de resposta
         btnListElement.innerHTML = "";
+        // Adiciona botões de resposta com event listeners
         pergunta.options.forEach(function (option, index) {
             var button = document.createElement("button");
             button.className = "btn-answer";
             button.textContent = option;
             btnListElement.appendChild(button);
+            // Adiciona um event listener para verificar a resposta ao clicar em um botão
             button.addEventListener("click", function () {
-                checkAnswer(index);
-                // Remove a cor verde de todos os botões
+                // Chama a função verificaR para atualizar a opção selecionada
+                verificaR(index);
+                // Remove a cor de todos os botões antes de destacar o botão selecionado
                 btnListElement.querySelectorAll(".btn-answer").forEach(function (btn) {
                     var buttonElement = btn;
-                    buttonElement.style.backgroundColor = "";
+                    buttonElement.style.backgroundColor = "#D8DDEA";
                 });
-                button.style.backgroundColor = "#B4B7FA";
+                // Destaca o botão selecionado com a cor desejada.
+                button.style.backgroundColor = "#8D818C";
             });
         });
+        // Torna o botão visível
         nextBtnElement.classList.remove("hide");
+        // Adiciona um event listener ao botão para verificar a resposta ao clicar
         nextBtnElement.addEventListener("click", function () {
+            // Verifica se uma opção foi selecionada antes de avançar
             if (selectedOptionIndex !== null) {
                 var perguntaAtual = pergunta;
+                // Chama a função verificarResposta para validar a resposta
                 verificarResposta(selectedOptionIndex, perguntaAtual.correct);
             }
             else {
+                // Exibe um alerta se nenhuma opção foi selecionada
                 alert("Por favor, selecione uma opção antes de responder.");
             }
         });
     }
 }
-function checkAnswer(index) {
+// Função para verificar a resposta selecionada
+function verificaR(index) {
+    // Verifica se uma opção já estava selecionada
     if (selectedOptionIndex !== null) {
+        // Obtém o botão anteriormente selecionado
         var prevSelectedOption = document.querySelector(".btn-answer[data-index=\"".concat(selectedOptionIndex, "\"]"));
+        // Remove a cor de destaque do botão anteriormente selecionado
         if (prevSelectedOption) {
             prevSelectedOption.style.backgroundColor = "";
         }
     }
+    // Atualiza a opção selecionada
     selectedOptionIndex = index;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 const renderPages = (pageName: string): void => {
+  // Realiza uma requisição fetch para carregar o conteúdo da página HTML
   fetch(`pages/${pageName}.html`)
     .then((resp) => {
       if (!resp.ok) {
@@ -7,196 +8,236 @@ const renderPages = (pageName: string): void => {
       return resp.text();
     })
     .then((html) => {
+      // Obtém o elemento raiz onde a página será exibida
       const rootElem = document.getElementById("root") as HTMLElement;
-
       if (rootElem) {
+        // Substitui o conteúdo do elemento raiz pelo HTML carregado
         rootElem.innerHTML = html;
 
+        // Obtém o formulário de início (se existir)
         const formStart = document.getElementById(
           "form-start"
         ) as HTMLFormElement;
 
+        // Adiciona um ouvinte de evento para o formulário de início
         if (formStart) {
           formStart.addEventListener("submit", (event) => {
             event.preventDefault();
+            // Chama a função armazenarNome ao enviar o formulário
             armazenarNome();
           });
         }
-
         if (pageName === "quiz") {
           carregarPergunta();
           console.log("A página quiz está pronta para ser usada!");
         } else if (pageName === "leaderboard") {
           console.log("A página leaderboard está pronta para ser usada!");
-          const leaderboardContainer = document.querySelector(".container-podium");
+          const leaderboardContainer =
+            document.querySelector(".container-podium");
+          if (leaderboardContainer) {
+            leaderboardContainer.innerHTML = "";
+            const nomeUsuario = localStorage.getItem("usuario");
 
-  if (leaderboardContainer) {
-    // Limpar o conteúdo atual
-    leaderboardContainer.innerHTML = "";
+            if (nomeUsuario) {
+              // Cria um elemento para o usuário no primeiro lugar do pódio
+              const firstPlaceItem = document.createElement("div");
+              firstPlaceItem.className = "podium-item";
+              firstPlaceItem.innerHTML = `
+                <div class="podium-rounded first-podium">${nomeUsuario
+                  .charAt(0)
+                  .toUpperCase()}${nomeUsuario.charAt(1).toUpperCase()}
+                </div>
+                <div>${nomeUsuario}</div>
+                <div>1/1</div>
+              `;
 
-    // Obter o nome do usuário do histórico local
-    const nomeUsuario = localStorage.getItem("usuario");
+              // Adiciona o elemento ao contêiner do pódio
+              leaderboardContainer.appendChild(firstPlaceItem);
+            } else {
+              console.error(
+                "Nome de usuário não encontrado no histórico local."
+              );
+            }
 
-    // Verificar se há um nome de usuário
-    if (nomeUsuario) {
-      // Criar um elemento para o usuário no primeiro lugar
-      const firstPlaceItem = document.createElement("div");
-      firstPlaceItem.className = "podium-item";
-      firstPlaceItem.innerHTML = `
-        <div class="podium-rounded first-podium">${nomeUsuario.charAt(0).toUpperCase()}</div>
-        <div>${nomeUsuario}</div>
-        <div>1/1</div>
-      `;
+            // Limpa a div "remaining-list"
+            const remainingList = document.querySelector(".remaining-list");
+            if (remainingList) {
+              remainingList.innerHTML = "";
+            } else {
+              console.error(
+                'Elemento com classe "remaining-list" não encontrado.'
+              );
+            }
+          } else {
+            console.error(
+              'Elemento com classe "container-podium" não encontrado.'
+            );
+          }
 
-      // Adicionar o elemento ao container-podium
-      leaderboardContainer.appendChild(firstPlaceItem);
-    } else {
-      console.error("Nome de usuário não encontrado no histórico local.");
-    }
-  } else {
-    console.error('Elemento com classe "container-podium" não encontrado.');
-  }
-}
-const novoJogo = document.querySelector(".btn-gold");
-        
-          // Adicionando um ouvinte de evento ao botão "Novo Jogo"
+          // Obtém o botão "Novo Jogo"
+          const novoJogo = document.querySelector(".btn-gold");
+
+          // Adiciona um ouvinte de evento ao botão "Novo Jogo"
           if (novoJogo) {
             novoJogo.addEventListener("click", () => {
               // Ao clicar no botão "Novo Jogo", retorna à página "start"
               renderPages("start");
             });
-          
-   
+          }
         }
-      } else {
-        console.error('Elemento com ID "root" não encontrado.');
       }
     })
     .catch((error) => {
+      // Trata erros durante a requisição fetch
       console.error("Erro durante a requisição fetch:", error);
     });
 };
-
-// Função para exibir mensagens
 renderPages("start");
 
- 
+// Função para armazenar o nome do usuário no localStorage
 function armazenarNome(): void {
-  const nomeUsuarioInput = document.getElementById("input-name") as HTMLInputElement | null;
+  // Obtém o elemento de input para o nome do usuário
+  const nomeUsuarioInput = document.getElementById(
+    "input-name"
+  ) as HTMLInputElement | null;
 
+  // Verifica se o elemento de input existe e se o valor não está vazio após remover espaços em branco
   if (nomeUsuarioInput && nomeUsuarioInput.value.trim() !== "") {
+    // Obtém o valor do nome do usuário
     const nomeUsuario = nomeUsuarioInput.value;
+
+    // Armazena o nome do usuário no localStorage
     localStorage.setItem("usuario", nomeUsuario);
     console.log("Nome armazenado com sucesso!");
     renderPages("quiz");
   } else {
     alert("Por favor, digite seu nome antes de começar.");
-    }
   }
-
+}
 
 function carregarPergunta(): void {
   fetch("questions.json")
     .then((resp) => {
+      // Verifica se a resposta da requisição foi bem-sucedida
       if (!resp.ok) {
         throw new Error("Erro na requisição fetch.");
       }
+      // Converte o conteúdo da resposta para formato JSON
       return resp.json();
     })
     .then((data) => {
+      // Exibe no console os dados recebidos do JSON
       console.log("Dados do JSON recebidos:", data);
+
+      // Atualiza o HTML com a primeira pergunta do conjunto de perguntas
       atualizarHTML(data.questions[0]);
     })
     .catch((error) => {
+      // Trata erros durante a requisição fetch
       console.error("Erro durante a requisição fetch:", error);
     });
 }
-function verificarResposta(selectedIndex: number, correctIndex: number): void {
-  const pontosPorRespostaCorreta = 1; // Defina a quantidade de pontos por resposta correta
 
+function verificarResposta(selectedIndex: number, correctIndex: number): void {
   if (selectedIndex === correctIndex) {
     alert("Resposta correta!");
-
-    // Adiciona pontos ao usuário
-    adicionarPontos(pontosPorRespostaCorreta);
   } else {
     alert("Resposta incorreta");
   }
-
   renderPages("leaderboard");
-}
-
-function adicionarPontos(pontos: number): void {
-  // Obtém a pontuação atual do usuário do localStorage ou inicializa com 0 se não existir
-  const pontuacaoAtual = parseInt(localStorage.getItem("pontuacao") || "0", 10);
-
-  // Adiciona os pontos
-  const novaPontuacao = pontuacaoAtual + pontos;
-
-  // Armazena a nova pontuação no localStorage
-  localStorage.setItem("pontuacao", novaPontuacao.toString());
 }
 
 let selectedOptionIndex: number | null = null;
 
+// Declaração do tipo Question
 interface Question {
   question: string;
   options: string[];
   correct: number;
 }
+
+// Função para atualizar o conteúdo HTML.
 function atualizarHTML(pergunta: Question): void {
+  // Obtém elementos HTML relevantes
   const questionNumberElement = document.querySelector(".question-number");
-  const questionContainerElement = document.querySelector(".question-container");
+  const questionContainerElement = document.querySelector(
+    ".question-container"
+  );
   const btnListElement = document.querySelector(".btn-list");
   const nextBtnElement = document.getElementById("next-btn");
 
-  if (questionNumberElement && questionContainerElement && btnListElement && nextBtnElement) {
-    questionNumberElement.textContent = "1/10";
+  // Verifica se os elementos existem
+  if (
+    questionNumberElement &&
+    questionContainerElement &&
+    btnListElement &&
+    nextBtnElement
+  ) {
+    // Define o número da pergunta atual
+    questionNumberElement.textContent = "1/1";
+
+    // Define o texto da pergunta
     questionContainerElement.textContent = pergunta.question;
+
+    // Limpa a lista de botões de resposta
     btnListElement.innerHTML = "";
 
+    // Adiciona botões de resposta com event listeners
     pergunta.options.forEach((option: string, index: number) => {
       const button = document.createElement("button");
       button.className = "btn-answer";
       button.textContent = option;
       btnListElement.appendChild(button);
 
+      // Adiciona um event listener para verificar a resposta ao clicar em um botão
       button.addEventListener("click", () => {
-        checkAnswer(index);
+        // Chama a função verificaR para atualizar a opção selecionada
+        verificaR(index);
 
-        // Remove a cor verde de todos os botões
+        // Remove a cor de todos os botões antes de destacar o botão selecionado
         btnListElement.querySelectorAll(".btn-answer").forEach((btn) => {
           const buttonElement = btn as HTMLElement;
-          buttonElement.style.backgroundColor = "";
+          buttonElement.style.backgroundColor = "#D8DDEA";
         });
-        button.style.backgroundColor = "#B4B7FA";
-        
-      });  
+
+        // Destaca o botão selecionado com a cor desejada.
+        button.style.backgroundColor = "#8D818C";
+      });
     });
 
+    // Torna o botão visível
     nextBtnElement.classList.remove("hide");
+
+    // Adiciona um event listener ao botão para verificar a resposta ao clicar
     nextBtnElement.addEventListener("click", () => {
+      // Verifica se uma opção foi selecionada antes de avançar
       if (selectedOptionIndex !== null) {
         const perguntaAtual = pergunta;
+
+        // Chama a função verificarResposta para validar a resposta
         verificarResposta(selectedOptionIndex, perguntaAtual.correct);
       } else {
+        // Exibe um alerta se nenhuma opção foi selecionada
         alert("Por favor, selecione uma opção antes de responder.");
       }
     });
   }
 }
-function checkAnswer(index: number): void {
+
+// Função para verificar a resposta selecionada
+function verificaR(index: number): void {
+  // Verifica se uma opção já estava selecionada
   if (selectedOptionIndex !== null) {
+    // Obtém o botão anteriormente selecionado
     const prevSelectedOption = document.querySelector(
       `.btn-answer[data-index="${selectedOptionIndex}"]`
     ) as HTMLElement | null;
 
+    // Remove a cor de destaque do botão anteriormente selecionado
     if (prevSelectedOption) {
-      prevSelectedOption.style.backgroundColor = ""; 
+      prevSelectedOption.style.backgroundColor = "";
     }
   }
-   selectedOptionIndex = index;
+  // Atualiza a opção selecionada
+  selectedOptionIndex = index;
 }
-
-
-
